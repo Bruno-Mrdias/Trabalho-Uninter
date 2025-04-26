cd7464b26eb327fb5ac592554fa7577fb2f5e45d
Bruno Matheus <112029921+Bruno-Mrdias@users.noreply.github.com>
2025-04-26 18:15:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function mostrarMensagem() {
+    const mensagem = "Bem-vindo à minha página do tranalho UNINTER! Que seu dia seja ótimo,produtivo e cheio de aprendizados :)'z.";
+    document.getElementById("mensagem").textContent = mensagem;
+  }
+  
+  window.onload = function() {
+    const botao = document.getElementById("botaoMensagem");
+    botao.addEventListener("click", mostrarMensagem);
+  };
